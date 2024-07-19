fd8f26c166c7517b6d0077fdd0b08b87c08dfa1c
DSUN <dsun@hanmail.net>
2024-07-19 14:56:51 +0900

    2024-07-19_20

--- src/PasswordReset.razor.cs
+++ src/PasswordReset.razor.cs
@@ -177,7 +177,7 @@ namespace MetaFrm.Razor
                     this.timer.Stop();
                 }
 
-                this.InvokeStateHasChanged();
+                this.InvokeAsync(this.StateHasChanged);
             }
             catch (Exception)
             {
